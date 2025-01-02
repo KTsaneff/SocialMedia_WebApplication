eaa6e4eb14f3b6bbad21e6f04e9d1d338ff82043
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-01-02 14:44:47 +0200

    Creating-a-post-partial-view

--- LoopSocialApp/LoopSocialApp/Controllers/HomeController.cs
+++ LoopSocialApp/LoopSocialApp/Controllers/HomeController.cs
@@ -1,19 +1,27 @@
+using LoopSocialApp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoopSocialApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var allPosts = await _context.Posts
+                .Include(n => n.ApplicationUser)
+                .ToListAsync();
+
+            return View(allPosts);
         }
     }
 }
