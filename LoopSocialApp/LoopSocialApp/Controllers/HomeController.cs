29b411d13ec166c509c20d2062131e621ba16d32
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-01-06 22:03:51 +0200

    First-Post-Created-From-UI

--- LoopSocialApp/LoopSocialApp/Controllers/HomeController.cs
+++ LoopSocialApp/LoopSocialApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using LoopSocialApp.Data;
+using LoopSocialApp.Data.DataModels;
+using LoopSocialApp.ViewModels.Home;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,9 +21,35 @@ namespace LoopSocialApp.Controllers
         {
             var allPosts = await _context.Posts
                 .Include(n => n.ApplicationUser)
+                .OrderByDescending(n => n.DateCreated)
                 .ToListAsync();
 
             return View(allPosts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(PostViewModel post)
+        {
+            //Hardcode existing user
+            var userId = "00c185e1-7e41-4a01-9643-28ed5c8233ba";
+
+            //Create new post
+            var newPost = new Post
+            {
+                Content = post.Content,
+                ImageUrl = string.Empty,
+                ApplicationUserId = userId,
+                DateCreated = DateTime.UtcNow,
+                DateUpdated = DateTime.UtcNow,
+                NumberOfReposrts = 0
+            };
+
+            //Add post to database
+            await _context.Posts.AddAsync(newPost);
+            await _context.SaveChangesAsync();
+
+            //Redirect to home page
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ LoopSocialApp/LoopSocialApp/ViewModels/Home/PostViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LoopSocialApp.ViewModels.Home
+{
+    public class PostViewModel
+    {
+        public string Content { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
