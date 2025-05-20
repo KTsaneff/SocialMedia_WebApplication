4885028ceb3f05aa7baa9094fe6695acf3b87e86
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-05-20 12:50:53 +0300

    Initial Stories feature implemented with image URL support

---
+++ LoopSocialApp/LoopSocialApp.Data/DataModels/Story.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoopSocialApp.Data.DataModels
+{
+    public class Story
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        public DateTime DateCreated { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string ApplicationUserId { get; set; } = null!;
+
+        //Navigation properties
+        public virtual ApplicationUser ApplicationUser { get; set; } = null!;
+
+        public virtual ICollection<Like> Likes { get; set; } = new List<Like>();
+
+        public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+        public virtual ICollection<Favorite> Favorites { get; set; } = new List<Favorite>();
+    }
+}
---
+++ LoopSocialApp/LoopSocialApp/Controllers/StoriesController.cs
@@ -0,0 +1,57 @@
+﻿using LoopSocialApp.Data;
+using LoopSocialApp.Data.DataModels;
+using LoopSocialApp.ViewModels.Stories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoopSocialApp.Controllers
+{
+    public class StoriesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateStory(StoryVM storyVM)
+        {
+            var loggedInUserId = "00c185e1-7e41-4a01-9643-28ed5c8233ba";
+
+            var newStory = new Story
+            {
+                DateCreated = DateTime.UtcNow,
+                IsDeleted = false,
+                ApplicationUserId = loggedInUserId
+            };
+
+            if(storyVM.Image != null && storyVM.Image.Length > 0)
+            {
+                string rootFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+                if (storyVM.Image.ContentType.Contains("image"))
+                {
+                    string rootFolderImages = Path.Combine(rootFolderPath, "images/stories");
+                    Directory.CreateDirectory(rootFolderImages);
+
+                    string fileName = Guid.NewGuid() + Path.GetExtension(storyVM.Image.FileName);
+                    string filePath = Path.Combine(rootFolderImages, fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                        await storyVM.Image.CopyToAsync(stream);
+
+                    newStory.ImageUrl = "/images/stories/" + fileName;
+                }
+            }
+
+            await _context.Stories.AddAsync(newStory);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
