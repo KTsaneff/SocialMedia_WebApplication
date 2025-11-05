385f39d10a9ea6d0536d2cef777419f470451d66
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-11-05 20:26:07 +0200

    Add Stories ViewComponent and fix story thumbnails display

---
+++ LoopSocialApp/LoopSocialApp/ViewComponents/StoriesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using LoopSocialApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoopSocialApp.ViewComponents
+{
+    public class StoriesViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public StoriesViewComponent(AppDbContext context)
+        {   
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var allStories = await _context.Stories.Include(s => s.ApplicationUser).ToListAsync();
+            return View(allStories);
+        }
+    }
+}
