2bbd4d157e5631d39e92ea22596545becff8cdac
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-11-06 22:48:58 +0200

    Defining Post Service Contract

---
+++ LoopSocialApp/LoopSocialApp.Data/Services/Interfaces/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using LoopSocialApp.Data.DataModels;
+using Microsoft.AspNetCore.Http;
+
+namespace LoopSocialApp.Data.Services.Interfaces
+{
+    public interface IPostService
+    {
+        Task<List<Post>> GetAllPostsAsync(string loggedInUserId);
+        Task<Post> CreatePostAsync(Post post, IFormFile image);
+        Task RemovePostAsync(int postId);
+        Task AddPostCommentAsync(Comment coment);
+        Task RemovePostCommentAsync(int commentId);
+        Task TogglePostLikeAsync(int postId, string userId);
+        Task TogglePostFavoritesAsync(int postId, string userId);
+        Task ReportPostAsync(int postId, string userId);
+    }
+}
---
+++ LoopSocialApp/LoopSocialApp.Data/Services/PostService.cs
@@ -0,0 +1,56 @@
+﻿using LoopSocialApp.Data.DataModels;
+using LoopSocialApp.Data.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace LoopSocialApp.Data.Services
+{
+    public class PostService : IPostService
+    {
+        private AppDbContext _context;
+
+        public PostService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddPostCommentAsync(Comment coment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> CreatePostAsync(Post post, IFormFile image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Post>> GetAllPostsAsync(string loggedInUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePostAsync(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemovePostCommentAsync(int commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReportPostAsync(int postId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TogglePostFavoritesAsync(int postId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TogglePostLikeAsync(int postId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
