f432dfd01fbaf66c1e12c70b1cd4b71027b25e45
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-11-06 14:20:50 +0200

    Updated ViewComponent to Show Hashtag

--- LoopSocialApp/LoopSocialApp/ViewComponents/HashtagsViewComponent.cs
+++ LoopSocialApp/LoopSocialApp/ViewComponents/HashtagsViewComponent.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LoopSocialApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoopSocialApp.ViewComponents
 {
     public class HashtagsViewComponent : ViewComponent
     {
+        private readonly AppDbContext _context;
+
+        public HashtagsViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var top5recentHashtags = await _context.Hashtags
+                .Where(h => h.DateCreated >= DateTime.UtcNow.AddDays(-7))
+                .OrderByDescending(h => h.Count)
+                .Take(5).ToListAsync();
+
+            return View(top5recentHashtags);
         }
     }
 }
