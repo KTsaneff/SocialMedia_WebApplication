fbdc0683a0c9e331118956ed7628d3470eedb2a3
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-11-06 11:24:39 +0200

    Converted Hashtags Partial View to View Component

---
+++ LoopSocialApp/LoopSocialApp/ViewComponents/HashtagsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoopSocialApp.ViewComponents
+{
+    public class HashtagsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
