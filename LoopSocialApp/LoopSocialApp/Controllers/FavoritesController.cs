264af7baaa673735e190ff08e6c756354000b09f
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2025-11-10 13:12:45 +0200

    Favorite Posts - Created Controller, Index Action and default View

---
+++ LoopSocialApp/LoopSocialApp/Controllers/FavoritesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoopSocialApp.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
